4be1096442442f6091ecb330a68b75f6cefb5110
Popper, Roman (SHS TE DC SVK D&A IIT 2) <roman.popper@siemens-healthineers.com>
2025-11-13 09:42:51 +0100

    Move random quest assignement for players to players' creation

--- src/app/players/players.service.ts
+++ src/app/players/players.service.ts
@@ -1,17 +1,43 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Player } from './player';
+import { QuestsService } from '../quests/quests.service';
+import { Quest } from '../quests/quest';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayersService {
+  private readonly questsService = inject(QuestsService);
+  private readonly availableQuests: Quest[];
+  private readonly players: Player[];
 
-  private players:Player[] = [
-    { id: 1, nickname: 'Klingacik', xp: 500, quests: [] },
-    { id: 2, nickname: 'SideEffectss', xp: 200, quests: []},
-  ];
+  constructor() {
+    this.availableQuests = this.questsService.getQuests();
+    this.players = this.createDefaultPlayers();
+  }
 
   getPlayers(): Player[] {
-      return this.players;
+    return [...this.players];
+  }
+
+  private createDefaultPlayers(): Player[] {
+    const defaultPlayerData = [
+      { id: 1, nickname: 'Klingacik', xp: 500 },
+      { id: 2, nickname: 'SideEffectss', xp: 200 }
+    ];
+
+    return defaultPlayerData.map(playerData => ({
+      ...playerData,
+      quests: [this.getRandomQuest()]
+    }));
+  }
+
+  private getRandomQuest(): Quest {
+    if (this.availableQuests.length === 0) {
+      throw new Error('No quests available to assign');
+    }
+    
+    const randomIndex = Math.floor(Math.random() * this.availableQuests.length);
+    return { ...this.availableQuests[randomIndex] };
   }
 }
--- src/app/players/players.ts
+++ src/app/players/players.ts
@@ -3,7 +3,6 @@ import { PlayersService } from './players.service';
 import { Player } from './player';
 import { MatList } from '@angular/material/list';
 import { PlayerItem } from "./player-item/player-item";
-import { QuestsService } from '../quests/quests.service';
 
 @Component({
   selector: 'app-players',
@@ -13,19 +12,6 @@ import { QuestsService } from '../quests/quests.service';
 })
 export class Players {
   private playersService = inject(PlayersService);
-  private questsService = inject(QuestsService);
 
   playersSignal = signal<Player[]>(this.playersService.getPlayers());
-
-  constructor(){
-    this.assignQuestToPlayersRandom();
-  }
-
-  private assignQuestToPlayersRandom(): void {
-    let quests = this.questsService.getQuests();
-    this.playersSignal.update(players => players.map(p => {
-      const randomIndex = Math.floor(Math.random() * quests.length);
-      return {...p, quests: [quests[randomIndex]]};
-    }));
-  }
 }
